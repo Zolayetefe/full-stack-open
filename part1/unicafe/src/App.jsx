1e4e1a2e808198abc0c3c1b35fd1b51b877e4493
zelalem tefera <zedoyetefe@gmail.com>
2026-09-26 13:37:48 +0300

    implement step 5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -22,16 +22,27 @@ function App() {
         <h1>give feedback</h1>
       </div>
       <div>
-        <button onClick={handleGood}>good</button>
-        <button onClick={handleNeutral}>neutral</button>
-        <button onClick={handleBad}>bad</button>
+       <Button onClick={handleGood} text="good"/>
+       <Button onClick={handleNeutral} text= "neutral"/>
+       <Button onClick = {handleBad} text="bad"/>
       </div>
       <Statistics bad={bad} good={good} neutral={neutral} />
     </>
   );
 }
 
+const Button = (props)=>{
+  console.log("button clicked....")
+  return (
+    <>  
+  <button onClick={props.onClick}>{props.text}</button>
+</>
+  )
+}
+
+
 const Statistics = (props) => {
+  console.log(props)
   if (props.bad + props.good + props.neutral > 0) {
     return (
       <>
@@ -40,32 +51,51 @@ const Statistics = (props) => {
             <h1>statistics</h1>
           </div>
           <div>
-            <p>good {props.good}</p>
-            <p>neutral {props.neutral}</p>
-            <p>bad {props.bad}</p>
-            <p>all {props.bad + props.neutral + props.good}</p>
-            <p>
-              average{" "}
-              {(props.good - props.bad) /
-                (props.good + props.bad + props.neutral)}
-            </p>
-            <p>
-              positive{" "}
-              {(props.good * 100) / (props.good + props.bad + props.neutral)}%
-            </p>
+            <StatisticLine text="good" value={props.good} />
+            <StatisticLine text="neutral" value={props.neutral} />
+            <StatisticLine text="bad" value={props.bad} />
+            <StatisticLine
+              text="all"
+              value={props.bad + props.neutral + props.good}
+            />
+            <StatisticLine
+              text="average"
+              value={
+                (props.good - props.bad) /
+                (props.good + props.bad + props.neutral)
+              }
+            />
+
+            <StatisticLine
+              text="positive"
+              value={
+                (
+                  (props.good * 100) /
+                  (props.good + props.bad + props.neutral)
+                ).toString() +
+                " " +
+                "%"
+              }
+            />
           </div>
         </div>
       </>
     );
-  }
-  else {
+  } else {
     return (
-       <>
-      <p>No feedback given</p>
+      <>
+        <p>No feedback given</p>
       </>
-    )
-     
-    
+    );
   }
 };
+
+const StatisticLine = (props) => (
+   <>
+    <p>
+      {props.text} {props.value}
+    </p>
+  </>
+)
+
 export default App;
